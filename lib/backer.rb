1fe44c3c4e2a50767542fcae6da1223daf18a1ad
Laura Darlak <laura@polyporedesign.com>
2018-09-04 17:01:29 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,6 +12,8 @@ class Backer
   def back_project(project)
     @backed_projects << project
     project.backers << self
+    # add the instance directly to the variable to avoid
+    # entering into a loop
   end
 
 end
